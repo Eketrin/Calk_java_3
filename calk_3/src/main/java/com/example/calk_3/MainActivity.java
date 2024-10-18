6a76f7f2c958e130fd0af14ba7319e37a3efdeea
Eketrin <Katebaiko@yandex.ru>
2024-10-19 00:36:10 +0300

    Работают кнопки

--- calk_3/src/main/java/com/example/calk_3/MainActivity.java
+++ calk_3/src/main/java/com/example/calk_3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calk_3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText inputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inputText = findViewById(R.id.InputText);
+    }
+
+    public void clicKNumber(View view) {
+        String number = inputText.getText().toString();
+        if(view.getId() == R.id.Zero)
+        {
+            number += "0";
+        }else if(view.getId() == R.id.One)
+        {
+            number += "1";
+        }else if(view.getId() == R.id.Two)
+        {
+            number += "2";
+        }else if(view.getId() == R.id.Three)
+        {
+            number += "3";
+        }else if(view.getId() == R.id.Four)
+        {
+            number += "4";
+        }else if(view.getId() == R.id.Five)
+        {
+            number += "5";
+        }else if(view.getId() == R.id.Six)
+        {
+            number += "6";
+        } else if(view.getId() == R.id.Seven)
+        {
+            number += "7";
+        } else if (view.getId() == R.id.Eight)
+        {
+            number += "8";
+        }else if (view.getId() == R.id.Nine)
+        {
+            number += "9";
+        }
+        inputText.setText(number);
     }
 }
