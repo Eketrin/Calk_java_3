f5dcd3a696d623ad66a9e87eece833ccd1e09bee
Eketrin <Katebaiko@yandex.ru>
2024-10-19 02:57:47 +0300

    есть ошибки, но +- точно работает

--- calk_3/src/main/java/com/example/calk_3/MainActivity.java
+++ calk_3/src/main/java/com/example/calk_3/MainActivity.java
@@ -12,7 +12,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String oldNumber;
+    String operator = "";
     EditText inputText;
+    Boolean isNew = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clicKNumber(View view) {
+        if (isNew){
+            inputText.setText("");
+        }
+        isNew=false;
+
+
+
         String number = inputText.getText().toString();
         if(view.getId() == R.id.Zero)
         {
@@ -59,7 +69,108 @@ public class MainActivity extends AppCompatActivity {
         }else if (view.getId() == R.id.Nine)
         {
             number += "9";
+        }else if (view.getId() == R.id.PlusMinus)
+        {
+            if(numberIsZero(number)){
+                number = "0";
+            }else {
+                number = (Double.parseDouble(number)*(-1)) + "";
+            }
+
+        }else if (view.getId() == R.id.Point)
+        {
+            if(!dotIsPresent(number)){ //была ли у нас раньше точка
+                number += ".";
+            }
         }
         inputText.setText(number);
+    }
+
+    public boolean numberIsZero(String numder){
+        if(numder.equals("0") || numder.equals("")){
+            return true;
+        }else {
+            return  false;
+        }
+    }
+    //была ли у нас раньше точка
+    public boolean dotIsPresent(String numder){
+        if(numder.indexOf(".") == -1 ){
+            return false;
+        }else {
+            return true;}
+    }
+    public void Operations(View view) {
+        isNew=true;
+        oldNumber = inputText.getText().toString();
+        if(view.getId() == R.id.Plus)
+        {
+            operator = "+";
+        }else if(view.getId() == R.id.Minus)
+        {
+            operator = "-";
+        }else if(view.getId() == R.id.Delenie)
+        {
+            operator = "/";
+        }else if(view.getId() == R.id.Umnojenie)
+        {
+            operator = "*";
+        }
+    }
+
+    public void clickResult(View view) {
+
+        String newNumber = inputText.getText().toString();
+        Double result = 0.0;
+        if(operator == "+")
+        {
+            result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber);
+        }else if(operator == "-")
+        {
+            result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber);
+        }else if(operator == "/")
+        {
+            result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber);
+        }else if(operator == "*")
+        {
+            result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber);
+        }
+
+        inputText.setText(result+"");
+    }
+
+    public void acClick(View view) {
+        inputText.setText("0");
+        oldNumber = "";
+        isNew = true;
+    }
+
+    public void clickProcent(View view) {
+        if (operator == ""){
+            String number = inputText.getText().toString();
+            double temp = Double.parseDouble(number)/100; //если 1 число
+            number = temp+"";
+            inputText.setText(number);
+        }else{
+            String newNumber = inputText.getText().toString();
+            Double result = 0.0;
+            Double Onum = Double.parseDouble(oldNumber);
+            Double Nnum = Double.parseDouble(newNumber);
+            if(operator == "+")
+            {
+                result = Onum + Nnum * Onum/100;
+            }else if(operator == "-")
+            {
+                result = Onum - Nnum * Onum/100;
+            }else if(operator == "/")
+            {
+                result = Onum / Nnum * 100;
+            }else if(operator == "*")
+            {
+                result = Onum * Nnum /100;
+            }
+            inputText.setText(result+"");
+            operator = "";
+        }
     }
 }
